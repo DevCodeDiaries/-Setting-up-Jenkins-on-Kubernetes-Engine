5e18102bfa92ea5fd2fcbb51d7e0418c3a89612c
DevCodeDiaries <smrutigupta403@gmail.com>
2025-06-14 14:09:08 +0530

    Update devcode117.sh

--- devcode117.sh
+++ devcode117.sh
@@ -1,29 +1,26 @@
-gcloud auth list
-
 export PROJECT_ID=$(gcloud config get-value project)
-
-export PROJECT_ID=$DEVSHELL_PROJECT_ID
-
-export ZONE=us-east1-c  # Replace with your assigned zone
+export ZONE=us-east1-c
 gcloud config set compute/zone $ZONE
 
+# Clone sample repo
 git clone https://github.com/GoogleCloudPlatform/continuous-deployment-on-kubernetes.git
-
 cd continuous-deployment-on-kubernetes
 
+# Create GKE cluster
 gcloud container clusters create jenkins-cd \
   --num-nodes 2 \
   --zone=$ZONE \
   --scopes "https://www.googleapis.com/auth/projecthosting,cloud-platform"
 
-gcloud container clusters list
+# Authenticate kubectl with the cluster
+gcloud container clusters get-credentials jenkins-cd --zone=$ZONE
 
-gcloud container clusters get-credentials jenkins-cd
-
+# Verify connection
 kubectl cluster-info
 
+# Add Helm repo
 helm repo add jenkins https://charts.jenkins.io
-
 helm repo update
 
+# Install Jenkins via Helm
 helm upgrade --install -f jenkins/values.yaml myjenkins jenkins/jenkins
